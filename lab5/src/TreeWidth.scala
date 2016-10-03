eb8503ff16be4ac836e236430afca07ad9cffb28
Casper Schreiter <casper.schreiter@gmail.com>
2016-10-03 17:46:44 +0200

    really bad and not nearly done

--- lab5/src/TreeWidth.scala
+++ lab5/src/TreeWidth.scala
@@ -1,5 +1,49 @@
-
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
+import scala.io.Source
 
 object TreeWidth {
-  
+
+  def graphParse(filename: String): Map[Int, Node] = {
+
+    val graph: Map[Int, Node] = scala.collection.mutable.Map[Int, Node]()
+
+    for (line <- Source.fromFile(filename).getLines()) {
+      if (!line.startsWith("c") && !line.startsWith("p")) {
+        var ints = line.split(" ")
+        var from = ints(0).toInt
+        var fromNode: Option[Node] = graph.get(from)
+        
+        if (fromNode == None) {
+          graph.+=(from -> new Node(from, new ArrayBuffer[Int])) 
+        }
+        
+        for (s <- 1 until ints.length) {
+          var to = ints(s).toInt
+          
+          if (graph.keySet.contains(to)) {
+            
+          } else {
+            val list = new ArrayBuffer[Int]
+            list += from
+            val n =  new Node(to, list)
+            
+            
+            
+          }
+          
+        }
+        
+      }
+      println(line)
+    }
+
+    graph
+  }
+
+  class Node(val id: Int, val children: ArrayBuffer[Int]) {
+    
+
+  }
+
 }
